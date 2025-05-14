c94ea2958af133044474bcd78d68a81b0c0f8bf2
Kennatris <90862818+Kennatris@users.noreply.github.com>
2025-05-14 16:37:57 +0200

    Update AggregatedRow.java

--- src/AggregatedRow.java
+++ src/AggregatedRow.java
@@ -4,7 +4,7 @@ public class AggregatedRow {
     public double maxEffizienz;
     public double avgGewinn;
     public double range;
-    public int count;
+    public int count; // counter for average divider
 
     public AggregatedRow(String produktionsstandort, double minEffizienz, double maxEffizienz, double avgGewinn) {
         this.produktionsstandort = produktionsstandort;
